e6a462f08805e30628fc0c5e25f004cbe69fc2ab
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-14 16:31:14 +0400

    solved merge strings alternately

--- src/main/java/easy/string/Merge_Strings_Alternately.java
+++ src/main/java/easy/string/Merge_Strings_Alternately.java
@@ -1,4 +1,33 @@
 package easy.string;
 
 public class Merge_Strings_Alternately {
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("abc", "pqr"));
+    }
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder answer= new StringBuilder();
+        int index = word2.length();
+        int length = word2.length();
+        if(word1.length() < word2.length()) {
+            length = word1.length();
+            index = word1.length();
+        }
+        int indexA = 0;
+        int indexB = 0;
+        for(int i = 0; i < 2 * length; i++){
+            if(i % 2 == 0) {
+                answer.append(word1.charAt(indexA));
+                indexA++;
+            }else {
+                answer.append(word2.charAt(indexB));
+                indexB++;
+            }
+        }
+        if (index == word2.length()) {
+            answer.append(word1.substring(index));
+        } else {
+            answer.append(word2.substring(index));
+        }
+        return answer.toString();
+    }
 }
