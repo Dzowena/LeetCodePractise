772e81696ae6b31693a9dedb7d2f6d18dcf8ea61
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-18 11:53:03 +0400

    solved Fizz Buzz

--- src/main/java/easy/string/Fizz_Buzz.java
+++ src/main/java/easy/string/Fizz_Buzz.java
@@ -1,4 +1,24 @@
 package easy.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Fizz_Buzz {
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 && i % 5 == 0)
+                answer.add("FizzBuzz");
+            else if(i % 3 == 0)
+                answer.add("Fizz");
+            else if(i % 5 == 0)
+                answer.add("Buzz");
+            else answer.add(""+i);
+        }
+        return answer;
+    }
 }
