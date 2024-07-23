61363503d3abe65ac22ffb42dfb1d05e45359898
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-23 11:58:06 +0400

    solved assign cookies

--- src/main/java/easy/greedy/Assign_Cookies.java
+++ src/main/java/easy/greedy/Assign_Cookies.java
@@ -1,4 +1,26 @@
 package easy.greedy;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Assign_Cookies {
+    public static void main(String[] args) {
+
+    }
+
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int answer = 0, cookieIndex = 0 , childIndex = 0;
+        while(cookieIndex < s.length && childIndex < g.length){
+            if(g[childIndex] <= s[cookieIndex]){
+                answer++;
+                cookieIndex++;
+                childIndex++;
+            }else cookieIndex++;
+        }
+        return answer;
+    }
 }
