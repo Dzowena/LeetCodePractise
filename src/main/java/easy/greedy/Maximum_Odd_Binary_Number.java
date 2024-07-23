9c9156b3b6a521cb45d6fed28bff7016ab22fbef
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-23 15:19:37 +0400

    solved maximum odd binary number

--- src/main/java/easy/greedy/Maximum_Odd_Binary_Number.java
+++ src/main/java/easy/greedy/Maximum_Odd_Binary_Number.java
@@ -1,4 +1,23 @@
 package easy.greedy;
 
 public class Maximum_Odd_Binary_Number {
+    public static void main(String[] args) {
+
+    }
+
+    public static String maximumOddBinaryNumber(String s) {
+        StringBuilder sb = new StringBuilder();
+        int zero = 0, one = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '0')
+                zero++;
+            else one++;
+        }
+        for(int i = 0; i < one - 1; i++)
+            sb.append("1");
+        for(int i = 0; i < zero; i++)
+            sb.append("0");
+        sb.append("1");
+        return sb.toString();
+    }
 }
