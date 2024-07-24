aa5a23f686861da5e2d33475b2db480e4f8f2e21
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-24 17:33:53 +0400

    minimum sum of four digit number after splitting digits

--- src/main/java/easy/greedy/Minimum_Sum_Of_Four_Digit_Number_After_Splitting_Digits.java
+++ src/main/java/easy/greedy/Minimum_Sum_Of_Four_Digit_Number_After_Splitting_Digits.java
@@ -1,4 +1,20 @@
 package easy.greedy;
 
+import java.util.Arrays;
+
 public class Minimum_Sum_Of_Four_Digit_Number_After_Splitting_Digits {
+    public static void main(String[] args) {
+
+    }
+
+    public int minimumSum(int num) {
+        int[] arr = new int[4];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = num % 10;
+            num /= 10;
+        }
+        Arrays.sort(arr);
+        return (arr[0] * 10) + (arr[1] * 10) + arr[2] + arr[3];
+    }
+
 }
