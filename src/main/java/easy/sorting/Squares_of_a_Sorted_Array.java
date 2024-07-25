b0092612bc6e4f37ff3edf9a021a83430e85aad0
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-25 16:54:57 +0400

    solved squares if a sirted array

--- src/main/java/easy/sorting/Squares_of_a_Sorted_Array.java
+++ src/main/java/easy/sorting/Squares_of_a_Sorted_Array.java
@@ -1,4 +1,25 @@
 package easy.sorting;
 
 public class Squares_of_a_Sorted_Array {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        int idx = n-1;
+        int i=0,j=n-1;
+
+        while(i<=j){
+            if(Math.abs(nums[i])>Math.abs(nums[j])){
+                ans[idx--] = nums[i]*nums[i];
+                i++;
+            }else{
+                ans[idx--] = nums[j]*nums[j];
+                j--;
+            }
+        }
+        return ans;
+    }
 }
