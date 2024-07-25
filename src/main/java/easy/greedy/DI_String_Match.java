22c60c9d4d337b7b662a8a5da5510d8570d4ab58
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-25 11:22:24 +0400

    solved DI string match

--- src/main/java/easy/greedy/DI_String_Match.java
+++ src/main/java/easy/greedy/DI_String_Match.java
@@ -1,4 +1,24 @@
 package easy.greedy;
 
 public class DI_String_Match {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] diStringMatch(String s) {
+        int D = s.length(), I = 0, length = s.length();
+        int[] answer = new int[length + 1];
+        for(int i = 0; i < length; i++){
+            if(s.charAt(i) == 'D'){
+                answer[i] = D;
+                D--;
+            }else {
+                answer[i] = I;
+                I++;
+            }
+        }
+        answer[length] = D;
+        return answer;
+    }
+
 }
