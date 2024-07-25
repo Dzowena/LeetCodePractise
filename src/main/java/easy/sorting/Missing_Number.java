859ad2e5927d98e92bde69d9e146a125d496e16d
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-25 15:41:09 +0400

    solved missing number

--- src/main/java/easy/sorting/Missing_Number.java
+++ src/main/java/easy/sorting/Missing_Number.java
@@ -1,10 +1,17 @@
 package easy.sorting;
 
 
+import java.util.Arrays;
+
 public class Missing_Number {
     public static void main(String[] args) {
 
     }
 
-
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
+        int actualSum = Arrays.stream(nums).sum();
+        return (sum - actualSum);
+    }
 }
