c1fb4ac630fa0323de05352202aac8cf62908d95
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-25 10:57:21 +0400

    solved make array zero by subtracting equal amounts

--- src/main/java/easy/greedy/Make_Array_Zero_By_Subtracting_Equal_Amounts.java
+++ src/main/java/easy/greedy/Make_Array_Zero_By_Subtracting_Equal_Amounts.java
@@ -1,4 +1,26 @@
 package easy.greedy;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Make_Array_Zero_By_Subtracting_Equal_Amounts {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minimumOperations(int[] nums) {
+        int answer = 0;
+        Set<Integer> uniques = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(!uniques.contains(nums[i])){
+                if(nums[i] != 0){
+                    uniques.add(nums[i]);
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
 }
