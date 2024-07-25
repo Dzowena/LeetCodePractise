0343b3e67147cce2636808144aecb2de0bc60a6a
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-25 16:04:09 +0400

    solved maximum product difference between two pairs

--- src/main/java/easy/sorting/Maximum_Product_Difference_Between_Two_Pairs.java
+++ src/main/java/easy/sorting/Maximum_Product_Difference_Between_Two_Pairs.java
@@ -1,4 +1,14 @@
 package easy.sorting;
 
+import java.util.Arrays;
+
 public class Maximum_Product_Difference_Between_Two_Pairs {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] * nums[nums.length - 2]) - (nums[0] * nums[1]);
+    }
 }
