49fb569598cbcccf9530f0da132c9d8d2e3d965c
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-25 12:50:40 +0400

    solved largest perimeter triangle

--- src/main/java/easy/greedy/Largest_Perimeter_Triangle.java
+++ src/main/java/easy/greedy/Largest_Perimeter_Triangle.java
@@ -1,4 +1,19 @@
 package easy.greedy;
 
+import java.util.Arrays;
+
 public class Largest_Perimeter_Triangle {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,1,2};
+        System.out.println(largestPerimeter(arr));
+    }
+
+    public static int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = nums.length - 1; i >= 2; i--){
+            if(nums[i - 1] + nums[i - 2] > nums[i])
+                return nums[i - 2] + nums[i - 1] + nums[i];
+        }
+        return 0;
+    }
 }
