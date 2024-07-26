f087850088ec243b007360cee6f4314963ecbe4a
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-26 12:57:20 +0400

    solved special array with X elements greater than or equal to X

--- src/main/java/easy/sorting/Special_Array_With_X_Elements_Greater_Than_or_Equal_X.java
+++ src/main/java/easy/sorting/Special_Array_With_X_Elements_Greater_Than_or_Equal_X.java
@@ -1,4 +1,20 @@
 package easy.sorting;
 
+import java.util.Arrays;
+
 public class Special_Array_With_X_Elements_Greater_Than_or_Equal_X {
+    public static void main(String[] args) {
+
+    }
+
+    public int specialArray(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        if(nums[0] >= n)
+            return n;
+        for(int i = 1; i <= n; i++)
+            if(nums[n - i] >= i && (n - i - 1 < 0 || nums[n - i - 1] < i))
+                return i;
+        return -1;
+    }
 }
