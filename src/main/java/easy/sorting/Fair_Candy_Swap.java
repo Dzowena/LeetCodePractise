c9953bb4fc60901c2444fcd0dad5b8c0093af5bc
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-29 16:53:25 +0400

    solved fair candy swap

--- src/main/java/easy/sorting/Fair_Candy_Swap.java
+++ src/main/java/easy/sorting/Fair_Candy_Swap.java
@@ -1,4 +1,28 @@
 package easy.sorting;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Fair_Candy_Swap {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sum1 = 0, sum2 = 0, diff = 0;
+        HashSet<Integer> nums = new HashSet<>();
+        for(int i = 0; i < aliceSizes.length; i++) {
+            sum1 += aliceSizes[i];
+            nums.add(aliceSizes[i]);
+        }
+        for(int i = 0; i < bobSizes.length; i++)
+            sum2 += bobSizes[i];
+        diff = (sum1 - sum2) / 2;
+        for(int i = 0; i < bobSizes.length; i++){
+            int target = diff + bobSizes[i];
+            if(nums.contains(target))
+                return new int[]{target, bobSizes[i]};
+        }
+        return null;
+    }
 }
