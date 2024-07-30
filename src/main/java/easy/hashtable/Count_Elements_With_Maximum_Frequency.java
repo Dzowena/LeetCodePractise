014fef93e7d195be4ae4e41cb0f408040a4a8329
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-30 16:54:31 +0400

    solved count elements with maximum frequency

--- src/main/java/easy/hashtable/Count_Elements_With_Maximum_Frequency.java
+++ src/main/java/easy/hashtable/Count_Elements_With_Maximum_Frequency.java
@@ -1,4 +1,22 @@
 package easy.hashtable;
 
 public class Count_Elements_With_Maximum_Frequency {
+    public static void main(String[] args) {
+        System.out.println(maxFrequencyElements(new int[]{10,12,11,9,6,19,11}));
+    }
+
+    public static int maxFrequencyElements(int[] nums) {
+        int max = 0, index = 0;
+        int[] arr1 = new int[101];
+        for(int i = 0; i < nums.length; i++)
+            arr1[nums[i]]++;
+        for(int i = 0; i < 101; i++)
+            if(arr1[i] > max) {
+                max = arr1[i];
+                index = 1;
+            }
+            else if(arr1[i] == max)
+                index++;
+        return max * index;
+    }
 }
