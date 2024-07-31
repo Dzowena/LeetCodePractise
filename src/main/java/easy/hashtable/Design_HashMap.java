893a83f2018818447dc31d7a115a04a0dffb8852
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-31 17:05:29 +0400

    solved design hashmap

--- src/main/java/easy/hashtable/Design_HashMap.java
+++ src/main/java/easy/hashtable/Design_HashMap.java
@@ -1,4 +1,22 @@
 package easy.hashtable;
 
 public class Design_HashMap {
+    public static void main(String[] args) {
+
+    }
+
+//    int[] data;
+//    public MyHashMap() {
+//        data = new int[1000001];
+//        Arrays.fill(data, -1);
+//    }
+//    public void put(int key, int val) {
+//        data[key] = val;
+//    }
+//    public int get(int key) {
+//        return data[key];
+//    }
+//    public void remove(int key) {
+//        data[key] = -1;
+//    }
 }
