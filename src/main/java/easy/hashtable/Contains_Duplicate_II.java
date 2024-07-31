ca5b2e73e7db3b9d3fe97880b4b4e8f21d929a36
Dzowena <lukadzotsenidze1999@gmail.com>
2024-07-31 10:52:56 +0400

    solved contains duplicate 2

--- src/main/java/easy/hashtable/Contains_Duplicate_II.java
+++ src/main/java/easy/hashtable/Contains_Duplicate_II.java
@@ -1,4 +1,32 @@
 package easy.hashtable;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Contains_Duplicate_II {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> mp=new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            if(mp.containsKey(nums[i])){
+                if(Math.abs(i-mp.get(nums[i])) <= k) return true;
+            }
+            mp.put(nums[i], i);
+        }
+        return false;
+    }
 }
+
+//first solution
+//int i = 0;
+//HashSet<Integer> hset = new HashSet<>();
+//        for(int j = 0; j < nums.length; j++){
+//        if(!hset.add(nums[j]))
+//        return true;
+//        if(hset.size() >= k + 1)
+//        hset.remove(nums[i++]);
+//        }
+//                return false;
