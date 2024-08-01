cbce74fcddcc2482fd12cafe2a274f1da9608cc2
Dzowena <lukadzotsenidze1999@gmail.com>
2024-08-01 10:05:03 +0400

    solved redistribute characters to make all strings equal

--- src/main/java/easy/hashtable/Redistribute_Characters_to_Make_All_Strings_Equal.java
+++ src/main/java/easy/hashtable/Redistribute_Characters_to_Make_All_Strings_Equal.java
@@ -1,4 +1,22 @@
 package easy.hashtable;
 
 public class Redistribute_Characters_to_Make_All_Strings_Equal {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean makeEqual(String[] words) {
+            final int[] characterCount = new int[26];
+            for (String inputWord : words) {
+                for (char inputChar : inputWord.toCharArray()) {
+                    characterCount[inputChar - 'a']++;
+                }
+            }
+            for (int count : characterCount) {
+                if (count % words.length != 0) {
+                    return false;
+                }
+            }
+            return true;
+    }
 }
